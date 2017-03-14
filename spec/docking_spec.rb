14e3df2227f1934df48dcc55d0775ed0756d1073
Jack Bittiner <jack@Jacks-MacBook-Pro.local>
2017-03-14 15:16:56 +0000

    release_bike test refactored to one line

--- spec/docking_spec.rb
+++ spec/docking_spec.rb
@@ -1,7 +1,5 @@
 require 'docking_station.rb'
 
 describe DockingStation do
-  it 'expects response when method release_bike is called on instance of DockingStation' do
-    expect(subject).to respond_to :release_bike
-  end
+  it { is_expected.to respond_to :release_bike}
 end
