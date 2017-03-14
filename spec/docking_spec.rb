8cac8836e9c444786de6d2070454c192543e7e03
Jack Bittiner <jack@Jacks-MacBook-Pro.local>
2017-03-14 15:05:47 +0000

    Passed release_bike test

--- spec/docking_spec.rb
+++ spec/docking_spec.rb
@@ -1,4 +1,7 @@
 require 'docking_station.rb'
 
 describe DockingStation do
+  it 'expects response when method release_bike is called on instance of DockingStation' do
+    expect((DockingStation.new.release_bike).nil?).to eq false
+  end
 end
