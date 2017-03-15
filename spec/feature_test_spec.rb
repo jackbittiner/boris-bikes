766378461a984c5614dffa8950891178d07292e9
Jack Bittiner <jack@Jacks-MacBook-Pro.local>
2017-03-15 13:52:23 +0000

    Docking station test for release working bike

--- spec/feature_test_spec.rb
+++
@@ -1,8 +0,0 @@
-require './lib/docking_station.rb'
-require './lib/bike.rb'
-
-describe 'bike release' do
-  it 'creates and returns new instance of bike when release_bike is called on DockingStation' do
-    expect((DockingStation.new.release_bike).class).to eq Bike
-  end
-end
