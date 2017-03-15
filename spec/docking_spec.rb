58013194f7824350cab84951a0b8274905d5534f
Jack Bittiner <jack@Jacks-MacBook-Pro.local>
2017-03-15 16:51:53 +0000

    Raising exceptions and expecting errors when no bike

--- spec/docking_spec.rb
+++ spec/docking_spec.rb
@@ -3,9 +3,19 @@ require 'docking_station.rb'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike}
 
-  it 'releases working bikes' do
-    bike = subject.release_bike # sets up test to create subject
-    expect(bike).to be_working
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    describe '#release_bike' do
+    it 'raises an error when bike method called on docking station equals nil' do
+      expect {subject.release_bike}.to raise_error "No bikes available"
+    end
+  end
+
   end
 
   # docking_station.dock(bike)
@@ -31,5 +41,7 @@ describe DockingStation do
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
+
+
 
 end
